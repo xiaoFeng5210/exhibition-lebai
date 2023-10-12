acf9b4f0ae99b2f9189de9c294ea83cbf5eeb3bb
Eadweard <804875728@qq.com>
2023-10-12 16:25:36 +0800

    图片上传

--- next.config.js
+++ next.config.js
@@ -2,6 +2,21 @@
 const nextConfig = {
   compiler: {
     styledComponents: true
+  },
+  webpack: (config) => {
+    return {
+      ...config,
+      devServer: {
+        hot:true, // 它是热更新：只更新改变的组件或者模块，不会整体刷新页面
+        proxy: { // 配置代理（只在本地开发有效，上线无效）
+          '/api': {
+            target: 'http://192.168.3.64:8092', // 这是本地用node写的一个服务，用webpack-dev-server起的服务默认端口是8080
+            pathRewrite: {"/api" : ""}, // 后台在转接的时候url中是没有 /api 的
+            changeOrigin: true, // 加了这个属性，那后端收到的请求头中的host是目标地址 target
+          },
+        } 
+      }
+    }
   }
 }
 
--- src/client-components/home-card/index.tsx
+++ src/client-components/home-card/index.tsx
@@ -3,13 +3,37 @@ import { SelectCard, CardStyle, Container, CardText, CardIconStyle } from "~/ass
 import { cardsInfo, type Card } from "~/constants"
 import Video from "../video"
 import { useRef } from "react"
+import { useToast } from '@chakra-ui/react'
 
 const HomeCard = () => {
+  const toast = useToast()
   const videoRef = useRef<typeof Video>(null)
-  const selectScene = (card: Card) => {
+  const fetchRunScene = async (card: Card) => {
+    const res = await fetch('/api/run_scene', {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({scene_type: card.type}),
+    }).catch(err => {
+      toast({
+        title: '系统错误',
+        description: "网络请求错误，请联系管理员",
+        status: 'error',
+        duration: 9000,
+        isClosable: true,
+      })
+      return err
+    })
+    return res.json()
+  }
+  const selectScene = async (card: Card) => {
     if (videoRef.current) {
       (videoRef.current as any).open(card)
     }
+    const res = await fetchRunScene(card)
+    console.log(res)
   }
   return (
     <Container>
