1de439a473ee2e5f96b69103c71582ae1f1bb4da
Eadweard <804875728@qq.com>
2023-10-12 10:06:32 +0800

    fix

--- src/assets/homepage-styles.tsx
+++ src/assets/homepage-styles.tsx
@@ -13,14 +13,19 @@ grid-template-columns: repeat(auto-fit, minmax(7.87rem, 1fr));
 export const CardStyle = styled.div`
 width: 7.87rem;
 height: 7.87rem;
-/* border: 8px solid #f8f8f8; */
 border-image: linear-gradient(deepskyblue, deeppink) 20 / 10px;
-border-radius: 50%;
 overflow: hidden;
 display: inline-flex;
 align-items: center;
 justify-content: center;
 `;
+
+export const CardChild = styled.div`
+width: 100%;
+height: 100%;
+background-color: white;
+border-radius: 50%;
+`
 
 export const CardText = styled.div`
 font-family: 'Verdana","arial","microsoft yahei","STXihei';
